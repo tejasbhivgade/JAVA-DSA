037184fdc3a694dcc89e293f3b9d5af454e5898b
tejasbhivgade <tejasbhivgade2001@gmail.com>
2024-08-31 18:57:27 +0530

    Various approches of printing array

--- DSA/src/com/Tejas/Array/PrintArray.java
+++ DSA/src/com/Tejas/Array/PrintArray.java
@@ -1,9 +1,34 @@
 package com.Tejas.Array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PrintArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int arr[]= {6,5,8,9,3,4,2,1,7};
+		
+
+		//======================== Approach 1 : Using tostring ==============================
+		
+		/*System.out.println(Arrays.toString(arr));*/
+		
+		
+		//======================== Approach 2 : Using for loop ==============================
+		
+		/*for (int i = 0; i < arr.length; i++) 
+		{
+			System.out.println(arr[i]);
+		}*/
+		
+		
+		//======================== Approach 3 : Using for each loop ==============================
+		
+		for (int i : arr) 
+		{
+			System.out.println(+i);
+		}
 
 	}
 
